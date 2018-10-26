2a1f91834c71c527cf1fd8f2a88212938514501c
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-10-26 10:42:37 -0600

    User story 2

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -3,4 +3,9 @@ class Machine < ApplicationRecord
 
   belongs_to :owner
   has_many :snacks
+
+  def average_price
+    found_snacks = Snack.where(id: snacks.ids)
+    found_snacks.average(:price).to_f
+  end
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,25 +1,34 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
-  scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
 
-    visit machine_path(dons)
+  before(:each)do
+    owner = Owner.create(name: "Sam's Snacks")
+    @dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
   end
-  scenario 'they see the snacks associated with that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+  scenario 'they see the location of that machine' do
+    visit machine_path(@dons)
+    expect(page).to have_content("Location: Don's Mixed Drinks")
+  end
 
-    dons.snacks.create(name: "White Castle Burger", price: 3.50)
-    dons.snacks.create(name: "Pop Rocks", price: 1.50)
-    dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
-    visit machine_path(dons)
+  scenario 'they see the snacks and prices associated with that machine' do
+    @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+    visit machine_path(@dons)
 
-    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
     expect(page).to have_content("White Castle Burger: $3.50")
     expect(page).to have_content("Pop Rocks: $1.50")
     expect(page).to have_content("Flaming Hot Cheetos: $2.50")
+  end
+
+  scenario 'they see the average price for all snacks associated with that machine' do
+    @dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    @dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    @dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+    visit machine_path(@dons)
+    expect(page).to have_content("Average Price: $2.50")
   end
 
 end
